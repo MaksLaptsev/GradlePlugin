4bb211de67b0171e48e1f632292a448868262124
Max Laptsev <maksru2009@gmail.com>
2023-10-09 16:23:49 +0300

    update code

--- plugin/src/main/java/gradleplugin/task/CreateTag.java
+++ plugin/src/main/java/gradleplugin/task/CreateTag.java
@@ -6,16 +6,23 @@ import gradleplugin.exeptions.UncommittedException;
 import gradleplugin.utils.Commands;
 import gradleplugin.utils.StringBuilderFromStream;
 import gradleplugin.utils.TagUtils;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.apache.logging.log4j.core.config.DefaultConfiguration;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.TaskAction;
-
 import java.io.IOException;
 
 public class CreateTag extends DefaultTask{
+    private static final Logger log = LogManager.getLogger(CreateTag.class);
     private Process process;
 
     public CreateTag() {
         this.setGroup("Custom gradle plugin homework");
+        Configurator.initialize(new DefaultConfiguration());
+        Configurator.setRootLevel(Level.INFO);
     }
 
     @TaskAction
@@ -28,7 +35,6 @@ public class CreateTag extends DefaultTask{
             isGitDir = Boolean.parseBoolean(out(Commands.COMMAND_IS_GIT_DIRECT)
                     .replace("\n","").split(" ")[0]);
             if (!isGitDir){
-                System.out.println("Is not a Git directory!!");
                 throw new GitNotFoundException("Is not a Git directory!!");
             }
 
@@ -36,17 +42,13 @@ public class CreateTag extends DefaultTask{
             isUncommitted_cache = !(out(Commands.COMMAND_DIFF_CACHE) == null || out(Commands.COMMAND_DIFF_CACHE).equals(""));
             if (isUncommitted || isUncommitted_cache){
                 String uncommittedTag = TagUtils.createUncommittedTag(out(Commands.COMMAND_UNCOMM_TAG));
-                System.out.println("There are uncommitted changes");
-                System.out.println(uncommittedTag);
-                throw new UncommittedException("There are uncommitted changes");
+                throw new UncommittedException("There are uncommitted changes\n"+uncommittedTag);
             }
 
             double currentTag = getCurrentTag(Commands.COMMAND_GET_CURRENT_TAG);
             isTagExist = currentTag != 0.0;
 
             if (isTagExist){
-                System.out.println("The tag already exists on the current commit");
-                System.out.println(currentTag);
                 throw new TaggedException("The tag already exists on the current commit!!");
             }
 
@@ -56,12 +58,12 @@ public class CreateTag extends DefaultTask{
             String newTag = TagUtils.createTag(lastTag,branch);
 
             process = Runtime.getRuntime().exec(Commands.COMMAND_CREATE_TAG+newTag);
-            System.out.println("new created tag: "+newTag);
+            log.info("new created tag: "+newTag);
 
             process = Runtime.getRuntime().exec(Commands.COMMAND_PUSH_TAG+branch+" "+newTag);
 
         }catch (GitNotFoundException | UncommittedException | TaggedException | IOException e){
-            System.out.println(e.getMessage());
+            log.warn(e.getMessage());
         }
 
     }
@@ -87,6 +89,7 @@ public class CreateTag extends DefaultTask{
         for (int i = 0; i < out.length; i++){
             if (out[i].equals("tag")){
                 tag = TagUtils.getNumericTag(out[i+1]);
+                return tag;
             }
         }
         return tag;
